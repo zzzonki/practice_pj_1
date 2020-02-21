255bc2136ac6ad80e00f25a5d321e16bbc0bfa35
zzzonki <zzzonki@gmail.com>
2020-02-22 02:59:28 +0300

    serv icons = done

---
+++ main.js
@@ -0,0 +1,12 @@
+let serv__pics = document.querySelectorAll(".serv__pic")
+
+for (let i = 0; i < serv__pics.length; i++) {
+        
+        serv__pics[0].style.backgroundImage = "url(../icons/map.png)"
+        serv__pics[1].style.backgroundImage = "url(../icons/souvenir.png)"
+        serv__pics[2].style.backgroundImage = "url(../icons/compass.png)"
+        serv__pics[3].style.backgroundImage = "url(../icons/picture.png)"
+        serv__pics[4].style.backgroundImage = "url(../icons/discussion.png)"
+        serv__pics[5].style.backgroundImage = "url(../icons/sun.png)"
+
+}
