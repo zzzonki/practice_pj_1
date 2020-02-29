a50763da52b90aad9e3bbb53b6f60d38bf6c9b3a
zzzonki <zzzonki@gmail.com>
2020-02-29 17:03:04 +0300

    full Photo section

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ for (let i = 0; i < serv__pics.length; i++) {
         serv__pics[5].style.backgroundImage = "url(../icons/sun.png)"
 
 }
+
+let photos_pics__item = document.querySelectorAll(".photos_pics__item")
+
+for (let i = 0; i < photos_pics__item.length; i++) {
+        
+        photos_pics__item[0].style.backgroundImage = "url(../photos/4.jpg)"
+        photos_pics__item[1].style.backgroundImage = "url(../photos/3.jpg)"
+        photos_pics__item[2].style.backgroundImage = "url(../photos/2.jpg)"
+        photos_pics__item[3].style.backgroundImage = "url(../photos/1.jpg)"
+        photos_pics__item[4].style.backgroundImage = "url(../photos/8.jpg)"
+        photos_pics__item[5].style.backgroundImage = "url(../photos/7.jpg)"
+        photos_pics__item[6].style.backgroundImage = "url(../photos/6.jpg)"
+        photos_pics__item[7].style.backgroundImage = "url(../photos/5.jpg)"
+
+}
